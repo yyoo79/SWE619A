b6bb5279a728d9aa0970f6b3f126c32eda0afce1
YangKyu Yoo <yangq79@gmail.com>
2017-09-06 09:41:10 -0400

    final assignment1 FindPrimeFactor with Chadia

--- src/assignment1/FindPrimeFactor.java
+++ src/assignment1/FindPrimeFactor.java
@@ -1,3 +1,4 @@
+// SWE619 Assignment1 by Yang Kyu Yoo and Chadia Habib
 package assignment1;
 
 import java.util.ArrayList;
@@ -5,25 +6,24 @@ import java.util.List;
 
 public class FindPrimeFactor {
 
-    public static void main(String[] args) {  
-    	//sample testing
-        List<Integer> testA = new ArrayList<Integer>();        
-        testA.add(1);
+    public static void main(String[] args) {
+       
+        List<Integer> testA = new ArrayList<Integer>();
+        testA.add(12);
+        testA.add(25);
         testA.add(18);
-        testA.add(-18);
-        testA.add(18);
-        testA.add(18);
-        testA.add(18);
+        testA.add(8);
         
-        List<Integer> testB = new ArrayList<Integer>();        
-        testB.add(1);
-        testB.add(-3);
+        List<Integer> testB = new ArrayList<Integer>();
+        testB.add(6);
+        testB.add(2);
         testB.add(3);
-        
-        System.out.println(findPrimeFactor(testA, testB));
-                        
+        testB.add(2);
+      
+        System.out.println(findPrimeFactor(testA,testB));        
+                
     }
-    
+
     // *********************************************************************************
     // Yang Kyu Yoo's Explanation
     // 1. both a and b cannot be empty because there is some index i where matches between b[i] and a[i]
@@ -32,6 +32,18 @@ public class FindPrimeFactor {
     // 4. case2 - [12, 25, 18, 8] and [6, 2, -3, 2] - negative number in 'b' still satisfy the precondition.
     // *********************************************************************************
     
+    // *********************************************************************************
+    // Chadia Habib's Explanation
+    // NOTE: we would normally also check  && i < a.size() in below for loop
+    // but considering requirement "there is some index i where b[i] is both prime and factor of a[i]"
+    // it's unnecessary to check size of both lists as i will be already found when we reach end of a[]
+    // could use for(int i=0; i<b.size() && i<a.size();i++) but unnecessary as per above explanation 
+    // NOTE: Chadia's note: if caller doesn't adhere to above condition and lists are of equal size function was returning 0. 
+    // Yoo's suggestion to address this issue was to return -1 in this scenario.
+    // NOTE: need to test case where prime factor is first, middle and end of list
+    // *********************************************************************************
+
+
     // Requires: a not null; b not null;
     //           there is some index i where b[i] is
     //           both prime and a factor of a[i]
@@ -44,26 +56,24 @@ public class FindPrimeFactor {
     //  3 is a prime factor of 18. Hence, index "2" is the correct
     //  answer.  index "3" is not a possible answer, because the
     //  third index is not the least index with the desired property.)
-    // Also note that a[] and b[] need not be of the same length.          
+    // Also note that a[] and b[] need not be of the same length.
+
 	public static int findPrimeFactor(List<Integer> a, List<Integer> b) {
-		
-		// looping array 'b' because array 'a' can be longer than 'b'
-		// once it's found return the value and exit out.
-		for (int i = 0; i < b.size(); i++) {
+		for (int i = 0; i < b.size() ; i++) {	
 			// Check factor AND check Prime
-			if ((a.get(i) % b.get(i) == 0) && isPrime(b.get(i))) {
+			if ((a.get(i) % b.get(i) == 0) && isPrime(b.get(i))) { 
 				return i;
 			}
 		}
-		return 0;
+		return -1;   //this means if caller doesn't adhere to requirements, it will get result -1 if lists are of equal size
 	}
 
 	private static boolean isPrime(Integer n) {
-		// prime number can't be negative and 0 and 1 are not prime number
+		// prime number can't be negative and 0 or 1 is not prime number
 		if (n <= 1) {
 			return false;
-		}
-		for (int i = 2; i < n; i++) {
+		}		
+		for (int i = 2; i < n; i++) {  
 			if (n % i == 0) {
 				return false;
 			}
