b3bd359d95f98d8a16af667dbf8959ed9416d47c
YangKyu Yoo <yangq79@gmail.com>
2017-09-19 08:48:39 -0400

    Assignment 3 - final

--- src/assignment3/GrowList.java
+++ src/assignment3/GrowList.java
@@ -1,135 +1,68 @@
 package assignment3;
+// SWE619 Assignment3 by 
+// 1. Michael Risher
+// 2. Chadia Habib
+// 3. Yang Kyu Yoo
 
 import java.util.*;
 
-//GrowList is a mutable list that only gets longer.
-public class GrowList<E> {
+public class GrowList  <E> {
 
-	private Map<Integer, E> values;
+	  private Map<Integer,E> values;
 
-	public GrowList() { values = new HashMap<Integer, E>(); }
-	
-	//Chadia's comment: this a new private constructor
-	/*
-	private GrowList(Map<Integer, E> val) {
-		values = new HashMap<Integer, E>();
-		values.putAll(val);
-	}
-	*/	
-	
-	// Yang's private constructor	
-	private GrowList(Map<Integer,E> val) {
-		this.values = val;
-	}
-		
-	//Chadia's comment: this is a mutator function need to change to make it mutable
-	// add to the end of the list
-	/*
-	public GrowList<E> add(E o) {
-		GrowList<E> GL = new GrowList<E>(values);
-		GL.values.put(size(), o);
-		return GL;
-	}
-	*/	
-	
-	// Yang's add function (producer)
-	// add to the end of the list	
-	public GrowList<E> add(E o) {
-		Map<Integer, E> newMap = new HashMap<Integer,E>(values);
-		newMap.put(size(), o);
-		return new GrowList<E>(newMap);
-	}
+	  public GrowList() { values = new HashMap<Integer,E>();}
+	  
+	  //private constructor
+	  private GrowList(Map<Integer, E> m) {
+		  values = m;
+	  }
 
-	// number of values in list
-	// Chadia's comment: this an observer and no need to modify
-	public int size() { return values.size(); }
+	  // add to the end of the list	  
+	  public GrowList<E> add(E o) {
+	     Map<Integer,E> copy = new HashMap<>(values);
+	     copy.put(size(), o);
+	     return new GrowList<E>(copy);
+	  }
 
-	// get ith value in list
-	// Chadia's comment: this an observer and no need to modify
-	public E get(int i) {
-		if (!inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
-		return values.get(i);
-	}
+	  // number of values in list
+	  public int size() { return values.size(); }
 
-	// update ith value in list;  return previous value or null if none
-	// Chadia's comment: this a mutator and observer, so we need to replace with 2
-	// different functions
-	// update ith value in list; return previous value or null if none
-	/*
-	public GrowList<E> set(int i, E o) {
-		if (!inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-		GrowList<E> GL = new GrowList<E>(values);
-		GL.values.put(i, o);
-		return GL;
-	}
-	*/
-	
-	// Yang's set fuction (producer)
-	// update ith value in list; return previous value or null if none	
-	public GrowList<E> set(int i, E o) {
-		if (!inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-		Map<Integer, E> newMap = new HashMap<Integer,E>(values);
-		if (newMap.containsKey(i)) {
-			newMap.put(i, o);
-			return new GrowList<E>(newMap);
-		}
-		return null;
-	}
-	
+	  // get ith value in list
+	  public E get(int i) { 
+	     if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
+	     return values.get(i);
+	  }
 
-	// private helper method
-	private boolean inRange(int i) {
-		return (i >= 0) && (i < size());
-	}
+	  // update ith value in list;
+	  public GrowList<E> set(int i, E o) {
+	     if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
+	     Map<Integer,E> copy = new HashMap<>(values);
+	     copy.put(i, o);
+	     return new GrowList<E>(copy);
+	  }
+	  
+	  // private helper method
+	  private boolean inRange(int i) { return (i >= 0) && (i < size()); }
 
-	public String toString() {
-		if (size() == 0)
-			return "[]";
-		String result = "[";
-		for (int i = 0; i < size() - 1; i++) {
-			result += values.get(i) + ",";
-		}
-		return result + values.get(size() - 1) + "]";
-	}
+	  public String toString() {
+	      if (size() == 0) return "[]";
+	      String result = "[";
+	      for (int i = 0; i < size()-1; i++) {
+	          result += values.get(i) + ",";
+	      }
+	      return result + values.get(size() - 1) + "]";
+	  }
 
-	public static void main(String[] args) {
-			
-		GrowList<String> list = new GrowList<String>();
-		System.out.println("list is:" + list);
-				
-		list = list.add("cat");
-		System.out.println("list is:" + list);
-		list = list.add("dog");
-		System.out.println("list is:" + list);
-		
-		// add empty string test
-		list = list.add("");
-		System.out.println("after empty string - list is:" + list);
-		
-		// add null test				
-		list = list.add(null);
-		System.out.println("after null - list is:" + list);
-				
-		list = list.set(1, "bat");
-		System.out.println("list.set(1, \"bat\") list is:" + list);
-		
-		// size		 
-		System.out.println("size() of list is:" + list.size());
-		
-		// get -1 : Expect IOBE
-		//System.out.println("list is:" + list.get(-1));
-		
-		// get 3
-		System.out.println("after get(3) - list is:" + list.get(3));
-		
-		// set -1 bat : Expect IOBE
-		//list = list.set(-1, "bat"); 
-		//System.out.println("list.set(-1, \"bat\") list is:" + list);
-		
-		// set 2 wasEmpty
-		list = list.set(2, "wasEmpty"); 
-		System.out.println("list.set(2, \"wasEmpty\"); list is:" + list);
-		
+	  public static void main(String[] args) {
+	     GrowList<String> list = new GrowList<String>();
 
-	}
+	     System.out.println("list is:" + list);
+	     list = list.add("cat");
+	     System.out.println("list is:" + list);
+	     list = list.add("dog");
+	     System.out.println("list is:" + list);
+	     list = list.set(1,"bat");
+	     System.out.println("list is:" + list);
+
+	  }
 }
--- src/assignment3/GrowListTest.java
+++ src/assignment3/GrowListTest.java
@@ -1,44 +1,50 @@
 package assignment3;
+// SWE619 Assignment3 by 
+// 1. Michael Risher
+// 2. Chadia Habib
+// 3. Yang Kyu Yoo
 
 import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.junit.*;
 
 public class GrowListTest {
+
 	
-	GrowList<String> list = new GrowList<String>();
-	
-	@Test
-	public void testAdd() {		
-		list = list.add("cat");
-		assertEquals(list.toString(), "[cat]");
+	@Test (expected=IndexOutOfBoundsException.class)
+	public void happyCreate() {
+		GrowList<String> g = new GrowList<>();
+		assertNotNull(g);
+		assertTrue(g.size()==0);
+		assertEquals("[]", g.toString());
+		g.get(1);
 	}
 	
 	@Test
-	public void testSet() {		
-		list = list.add("cat");
-		list = list.add("dog");
-		list = list.set(1, "bat");
-		//System.out.println("list.set(1, \"bat\") list is:" + list);
-		assertEquals(list.toString(), "[cat,bat]");
-	}
-	
-	@Test
-	public void testSize() {		
-		list = list.add("cat");
-		list = list.add("dog");
-		list = list.set(1, "bat");
-		//System.out.println("list.set(1, \"bat\") list is:" + list);
-		assertEquals(list.size(), 2);
-	}
-	
-	@Test
-	public void testGet() {		
-		list = list.add("cat");
-		list = list.add("dog");
-		list = list.set(1, "bat");
-		//System.out.println("list.get(0) list is:" + list.get(0));
-		assertEquals(list.get(0), "cat");
+	public void addElement() {
+		GrowList<String> g = new GrowList<>();
+		GrowList<String> f = g.add("cat");
+		GrowList<String> h = g.add("cat");
+		assertFalse(g==f);
+		assertFalse(f==h);
+		assertTrue(g.size()==0);
+		assertTrue(f.size()==1);
+		assertEquals(f.get(0), "cat");
+		assertEquals(f.toString(), "[cat]");
+		f=f.add("bird");
+		assertTrue(f.size()==2);
+		assertEquals(f.toString(), "[cat,bird]");
 	}
 
+	@Test
+	public void setElement() {
+		GrowList<String> g = new GrowList<>();
+		GrowList<String> f = g.add("cat");
+		GrowList<String> h = f.set(0,"dog");
+		assertFalse(g==f);
+		assertFalse(f==h);
+		assertTrue(h.size()==1);
+		assertTrue(f.size()==1);
+		assertEquals(f.get(0), "cat");
+		assertEquals(h.get(0), "dog");
+	}
 }
