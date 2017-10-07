33bba4687c8836d06e03d30eaf4ef9abd831e3b6
YangKyu Yoo <yangq79@gmail.com>
2017-10-06 20:12:10 -0400

    Assignment 5 final

--- src/assignment5/GrowList.java
+++ src/assignment5/GrowList.java
@@ -3,14 +3,147 @@ package assignment5;
 import java.util.*;
 
 //GrowList is a mutable list that only gets longer.
+public class GrowList<E> implements Iterable<E> {
+
+	private Map<Integer,E> values;
+	private long modCount = 0;
+
+	public GrowList() { 
+		values = new HashMap<Integer,E>();	
+	}
+
+	// add to the end of the list
+	public void add(E o) {
+		incrementMod();
+		values.put(size(), o);	
+	}
+
+	// number of values in list
+	public int size() { 
+		return values.size(); 
+	}
+
+	// get ith value in list
+	public E get(int i) { 
+		if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
+		return values.get(i);
+	}
+
+	// update ith value in list;  return previous value or null if none
+	public E set(int i, E o) {
+		if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
+		incrementMod();
+		return values.put(i, o);
+	}
+
+	// private helper method
+	private boolean inRange(int i) { 
+		return (i >= 0) && (i < size()); 
+	}
+
+	public String toString() {
+		if (size() == 0) return "[]";
+		String result = "[";
+		for (int i = 0; i < size()-1; i++) {
+			result += values.get(i) + ",";
+		}
+		return result + values.get(size() - 1) + "]";
+	}
+	
+	public boolean repOk() {
+		return (values != null && values.size() >= 0);
+	}
+
+	public static void main(String[] args) {
+		GrowList<String> list = new GrowList<String>();
+
+		System.out.println("list is:" + list);
+		list.add("cat");
+		System.out.println("list is:" + list);
+		list.add("dog");
+		System.out.println("list is:" + list);
+		list.set(1,"bat");
+		System.out.println("list is:" + list);
+
+	}
+	
+	private void incrementMod() {
+		modCount = (modCount+1) % Long.MAX_VALUE;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new GrowListIterator();
+	}
+	
+	private class GrowListIterator implements Iterator<E> {
+		
+		private long modCount = GrowList.this.modCount;
+		private Deque<E> values = new ArrayDeque<>(GrowList.this.values.values());
+		
+		public GrowListIterator() {}
+
+		@Override
+		public boolean hasNext() {
+			if(modCount != GrowList.this.modCount)
+				throw new ConcurrentModificationException();
+			return !values.isEmpty();
+		}
+
+		@Override
+		public E next() {
+			if(modCount != GrowList.this.modCount)
+				throw new ConcurrentModificationException();
+			if(values.isEmpty())
+				throw new NoSuchElementException();
+			return values.removeFirst();
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// my code below
+
+/*
+ 
+ 
+//GrowList is a mutable list that only gets longer.
 //public class GrowList<E> {
-public class GrowList<E> {
+public class GrowList <E> {
 	
 	//private Map<Integer,E> values;
 	private Map<Integer,E> values;
+	
+	public GrowList() { values = new HashMap<Integer,E>();}
+	 
+	// add to the end of the list
+	public void add(E o) {
+		values.put(size(), o);
+	}
+
+	// number of values in list
+	public int size() { return values.size(); }
 			
-	@SuppressWarnings("rawtypes")
-	public Iterator list( ) {
+	//@SuppressWarnings("rawtypes")
+	public Iterator GrowListIterator( ) {
 		//E: Returns a generator that will produce
 		//   the list that only gets longer
 		//R: this must not be modified while the generator is in use.
@@ -18,47 +151,20 @@ public class GrowList<E> {
 	}
 	
 	// inner class
-	@SuppressWarnings("rawtypes")
+	//@SuppressWarnings("rawtypes")
 	private class GrowListGen implements Iterator{
 		//private GrowList<E> gl;
 		private GrowList<E> gl;
-		private int n; //next nth item
+		private int pointer; //nth item
 		
 		GrowListGen (GrowList<E> it){
 			//Requires: it != null
 			gl = it;
 			//if (gl.values == 0)
-			n= 0;
+			pointer= 0;
 		}
 			
-		// add to the end of the list	
-		public GrowList<E> add(E o){
-			//Map<Integer,E> copy = new HashMap<>(values);			
-			gl.values.put(size(), o);
-			GrowList<E> glcopy = gl;
-			return glcopy;			
-		}
-		
-		// number of values in list
-		public int size() { return gl.values.size(); }
-		
-		// get ith value in list
-		public E get(int i) { 
-			if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
-			return gl.values.get(i);
-		}
-		
-		// update ith value in list;
-		public GrowList<E> set(int i, E o) {
-			if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-			//Map<Integer,E> copy = new HashMap<>(values);			
-			gl.values.put(i, o);
-			GrowList<E> glcopy = gl;
-			return glcopy;
-		}
-		
-		// private helper method
-		private boolean inRange(int i) { return (i >= 0) && (i < size()); }
+		// add to the end of the list			
 		
 		public String toString() {
 			if (size() == 0) return "[]";
@@ -70,71 +176,42 @@ public class GrowList<E> {
 		}
 				
 		
-		public boolean hasNext() {			
-			if (n < size()) {	
-				return true;
-			}
-			return false;						
+		public boolean hasNext() {
+			
+			//if (n < size()) {			
+			//	return true;
+			//}
+			//return false;
+			
+			return (pointer < size());
 		}
 		
+		
+		// Question on next() method 
+		// Now next() for GrowList will not remove item from the list due to the contract 'list only gets longer'
+				
 		public Object next() throws NoSuchElementException {
+			if (pointer == size()) {
+				return new NoSuchElementException("GrowListGen.next()");
+			}
 			//get nth E
-			E element = get(n);
-			//todo: implement this
-			Object result = null;
-			return result;
+			E element = gl.values.get(pointer);
+			
+			//reduce the size
+			//gl.values.remove(0);			
+			//move pointer
+			pointer++;
+			return element;
+		}
+		
+		public void remove() {
+			new UnsupportedOperationException("GrowList.remove");
 		}
 		
 	}
-	/*
-	public GrowList() { values = new HashMap<Integer,E>();}
-	 
-	  // add to the end of the list
-	  public void add(E o) {
-	     values.put(size(), o);
-	  }
 	
-	  // number of values in list
-	  public int size() { return values.size(); }
-	
-	  // get ith value in list
-	  public E get(int i) { 
-	     if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
-	     return values.get(i);
-	  }
-	
-	  // update ith value in list;  return previous value or null if none
-	  public E set(int i, E o) {
-	     if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-	     return values.put(i, o);
-	  }
-	  
-	  // private helper method
-	  private boolean inRange(int i) { return (i >= 0) && (i < size()); }
-	
-	  public String toString() {
-	      if (size() == 0) return "[]";
-	  String result = "[";
-	  for (int i = 0; i < size()-1; i++) {
-	      result += values.get(i) + ",";
-	  }
-	  return result + values.get(size() - 1) + "]";
-	  }
-	
-	  public static void main(String[] args) {
-	     GrowList<String> list = new GrowList<String>();
-	
-	     System.out.println("list is:" + list);
-	 list.add("cat");
-	 System.out.println("list is:" + list);
-	 list.add("dog");
-	 System.out.println("list is:" + list);
-	 list.set(1,"bat");
-	 System.out.println("list is:" + list);
-	
-	  }
-	  */
 }
+*/
 
 /*
 Goal: Iteration Abstraction, and a bit of Type Abstraction.
--- src/assignment5/GrowListTest.java
+++ src/assignment5/GrowListTest.java
@@ -1,14 +1,157 @@
 package assignment5;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.*;
+import java.util.Iterator;
 
 public class GrowListTest {
 
+	@Test(expected=UnsupportedOperationException.class)
+	public void happyPath() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.add("elephant");
+		Collection<String> seenValues = new Vector<>();
+		seenValues.add("bat");
+		seenValues.add("cat");
+		seenValues.add("dog");
+		seenValues.add("elephant");
+		
+		Iterator<String> iter = gl.iterator();
+		assertTrue(iter.hasNext());
+		System.out.println("iter.hasNext(): " + iter.hasNext());
+		String value = iter.next();
+		System.out.println("value: " + value);
+		System.out.println("seenValues: " + seenValues);
+		assertTrue(seenValues.contains(value));
+		seenValues.remove(value);
+		System.out.println("seenValues(after remove): " + seenValues);
+		assertTrue(iter.hasNext());
+		System.out.println("iter.hasNext(): " + iter.hasNext());
+		value = iter.next();
+		System.out.println("value: " + value);
+		System.out.println("seenValues: " + seenValues);
+		assertTrue(seenValues.contains(value));
+		seenValues.remove(value);
+		System.out.println("seenValues(after remove): " + seenValues);
+		assertTrue(iter.hasNext());
+		System.out.println("iter.hasNext(): " + iter.hasNext());
+		value = iter.next();
+		assertTrue(seenValues.contains(value));
+		seenValues.remove(value);
+		assertTrue(iter.hasNext());
+		value = iter.next();
+		assertTrue(seenValues.contains(value));
+		seenValues.remove(value);
+		assertFalse(iter.hasNext());
+		iter.remove();
+	}
+	
+	@Test (expected=NoSuchElementException.class)
+	public void goPastEndOfList() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.add("elephant");
+		Iterator<String> iter = gl.iterator();
+		while(iter.hasNext()) {
+			iter.next();
+		}
+		iter.next();
+		
+	}
+	
+	@Test (expected=ConcurrentModificationException.class)
+	public void concurrentModificationViaAdd() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.add("elephant");
+		Iterator<String> iter = gl.iterator();
+		while(iter.hasNext()) {
+			iter.next();
+			gl.add("zebra");
+		}
+	}
+	
+	@Test (expected=ConcurrentModificationException.class)
+	public void concurrentModificationViaSet() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.add("elephant");
+		Iterator<String> iter = gl.iterator();
+		while(iter.hasNext()) {
+			iter.next();
+			gl.set(3, "zebra");
+		}
+	}
+	
+	@Test (expected=ConcurrentModificationException.class)
+	public void concurrentModificationViaSet2() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.add("elephant");
+		Iterator<String> iter = gl.iterator();
+		gl.set(3, "zebra");
+		iter.next();
+	}
+	
+	@Test
+	public void setBeforeGetAfterIterator() {
+		GrowList<String> gl = new GrowList<>();
+		gl.add("bat");
+		gl.add("cat");
+		gl.add("dog");
+		gl.set(1, "sheep");		
+		Iterator<String> iter = gl.iterator();
+		String temp ="";
+		while(iter.hasNext()) {
+			iter.next();			
+			System.out.println("get(1): " + gl.get(1));
+		}
+	}
+	
+	/*
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		GrowList<String> list = new GrowList<String>();
+		
+		
+		//System.out.println("list is:" + list);
+		list.add("cat");
+		//System.out.println("list is:" + list);
+		list.add("dog");
+		//System.out.println("list is:" + list);
+		list.add("bat");
+		//System.out.println("list is:" + list);
+		
+//		list.set(1,"bat");
+//		System.out.println("list is:" + list);		
+
+		Iterator<?> itr = list.GrowListIterator();
+					
+	    System.out.println("list is: " + itr);
+	    System.out.println("itr.hasNext(): " + itr.hasNext());	    
+	    System.out.println("list is: " + itr);
+	    System.out.println("next() is : " + itr.next());
+	    System.out.println("list is: " + itr);
+	    System.out.println("next() is : " + itr.next());
+	    System.out.println("list is: " + itr);
+	    System.out.println("next() is : " + itr.next());
+	    System.out.println("list is: " + itr);
+	    System.out.println("itr.hasNext(): " + itr.hasNext());
+	    System.out.println("next() is : " + itr.next());
+	    	     
 	}
+	*/
 
 }
